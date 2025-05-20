66de700c703e048231795b6aedc810a07dd9c4e3
lukavekua452 <lukavekuaa2015@gmail.com>
2025-05-20 20:49:34 +0400

    “commit”

---
+++ day10/homework/main.py
@@ -0,0 +1,27 @@
+#type() აქვს იმის დანიშნულობა რომ მაგალითად ადამიანმა უნდა ჩაწეროს თავისი ნომერი.. გამოგადგება იმისთვის რომ შეამოწმო,გაასწორო მაგ.str to int.
+
+#string-str
+#integer-int
+#float-float
+#boolean-bool
+
+num=5
+print(input("Enter your Num: "))
+num= int(num)
+print(type(num))
+
+
+C=+15
+print(input("Enter C: "))
+C=int(C)
+print(type(num))
+
+surname="Vekua"
+print(input("Enter your surname: "))
+surname=str(surname)
+print(type(surname))
+
+Age=10
+print(input("Enter your age: "))
+Age=str(Age)
+print=(type(Age))
---
+++ day12/homework/main.py
@@ -0,0 +1,73 @@
+# > -მეტობა
+# < -ნაკლებობა
+# == -უდრის უდრისი
+# != -არ უდრის
+
+# >
+num1=10
+num=5
+print(10 > 5)
+
+num1=15
+num2=11
+print(15 > 11)
+
+num1=30
+num2=20
+print(30 > 20)
+
+# <
+num1=7
+num2=5
+print(7 < 5)
+
+num1=50
+num2=25
+print(50 < 25)
+
+num1=100
+num2=28
+print(100 < 28)
+
+
+# ==
+num1=70
+num2=70
+print(70==70)
+
+num1=76
+num2=76
+print(76 == 76)
+
+num1=700
+num2=700
+print(700 == 700)
+
+
+# !=
+
+num1=80
+num2=7
+print(80 != 7)
+
+num1=60
+num2=50
+print(60 != 50)
+
+num1=1000
+num2=500
+print(1000 != 500)
+
+my_height="5.4"
+height="5.5"
+print(5.4 == 5.5)
+
+#num1 = "14"
+#num2 = 14
+#print(num1 == num2) #ცვლადები ერთმანეთს არ ემთხვევა
+
+my_surname="vekua"
+surname="zarqua"
+print("vekua" == "vekua")
+
+
