207d77fc34fa6ae6b5e79b910b6faf4be92f07d2
lukavekua452 <lukavekuaa2015@gmail.com>
2025-05-28 07:16:14 +0400

    commit

---
+++ day01/homework/day01.py
@@ -0,0 +1,26 @@
+# ცვლადი არის სიტყვის მნიშვნელოვა,რომ უფრო იოლი გახდეს კოდის დაწერა.კომენტარს იმ დანიშნულებით ვიყენებთ რომ დაგვამახსოვრდეს თუ რა კოდი გავაკეთეთ და ასევე კარგი პრაქტიკაა.
+#string
+#integer
+#float
+#boolean
+
+print("Luka")
+print("Lazare")
+print("Mari")
+print("Liza")
+print("Giga")
+
+print(300)
+print(200)
+print(100)
+print(400)
+print(500)
+
+print(5.05)
+print(10.5)
+print(100.7)
+print(70.5)
+print(60.1)
+
+print(True)
+print(False)
---
+++ day06/classwork/main.py
@@ -0,0 +1,16 @@
+# კომენტარს იმ დანიშნულებით ვიყენებთ რომ დაგვამახსოვრდეს თუ რა კოდი გავაკეთეთ და ასევე კარგი პრაქტიკაა.
+#camel case and snake case
+
+
+first_user = "Lika"
+usernd2 = "Giorgi"
+print(first_user)
+
+first_name = "Data"
+last_name = "random"
+
+Num1 = 20
+num2 = 30
+print(Num1 + num2)
+
+#გამოიტანს 50-ს
