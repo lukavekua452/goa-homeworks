546094d078abb7a4abeb2f06de0fd2a5699ae8fa
lukavekua452 <lukavekuaa2015@gmail.com>
2025-05-30 19:51:59 +0400

    commit

--- day02/classwork/day02.py
+++ day02/classwork/day02.py
@@ -1,6 +1,11 @@
-num1=180
-num2=6
-print(180+6)
-print(180-6)
-print(180*6)
-print(180/6)      
+name="luka"
+surname="Vekua"
+print(name + surname)
+
+
+num1="8"
+num2="4"
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
